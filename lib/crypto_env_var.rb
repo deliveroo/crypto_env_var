7c3b766170e82912dc641a59dcc03f166e4b27a8
tom <tommaso@pavese.me>
2017-09-14 23:01:20 +0100

    Remove unneded module namespace

--- lib/crypto_env_var.rb
+++ lib/crypto_env_var.rb
@@ -6,22 +6,22 @@ require "crypto_env_var/utils"
 module CryptoEnvVar
   class << self
     def encrypt(data, private_key_string)
-      json = CryptoEnvVar::Utils.serialize(data)
+      json = Utils.serialize(data)
 
-      cipher = CryptoEnvVar::Cipher.new(private_key_string)
+      cipher = Cipher.new(private_key_string)
       encrypted_data = cipher.encrypt(json)
 
-      CryptoEnvVar::Utils.encode(encrypted_data)
+      Utils.encode(encrypted_data)
     end
 
 
     def decrypt(string, public_key_string)
-      encrypted_data = CryptoEnvVar::Utils.decode(string)
+      encrypted_data = Utils.decode(string)
 
-      cipher = CryptoEnvVar::Cipher.new(public_key_string)
+      cipher = Cipher.new(public_key_string)
       json = cipher.decrypt(encrypted_data)
 
-      CryptoEnvVar::Utils.deserialize(json)
+      Utils.deserialize(json)
     end
   end
 end
